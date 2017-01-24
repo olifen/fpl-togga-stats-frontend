866578884fe7ba7c29973927c6bd557c70ef8488
Oliver <of9142@my.bristol.ac.uk>
2017-01-24 18:37:36 +0000

    initial setup

---
+++ src/js/router.js
@@ -0,0 +1,24 @@
+angular.module('fplToggaStats')
+  .config(Router);
+
+Router.$inject = ['$stateProvider', '$urlRouterProvider'];
+function Router($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('usersIndex', {
+      url: '/users',
+      templateUrl: '/templates/usersIndex.html',
+      controller: 'UsersIndexController as usersIndex'
+    })
+    .state('register', {
+      url: '/register',
+      templateUrl: '/templates/register.html',
+      controller: 'RegisterController as register'
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: '/templates/login.html',
+      controller: 'LoginController as login'
+    });
+
+  $urlRouterProvider.otherwise('/users');
+}
